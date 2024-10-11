fd5d7e7fb5e80c5306c86f72078c4ac11337b2ac
Yash Janoria <yash.janoria@314ecorp.com>
2024-10-11 17:05:02 +0530

    feat: init repo

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { ChangeEvent, useMemo, useState } from "react";
+
+import "./App.css";
+import useSQLite from "./hooks/useSQLite";
+import { Table } from "antd";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [dbName, setSelectedDB] = useState("mydb.sqlite3");
+  const { data, isLoading, error, runQuery, updateDB } = useSQLite();
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const content = useMemo(() => {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+    if (error) {
+      return <div className="text-red-500">{error}</div>;
+    }
+    if (data) {
+      if (Array.isArray(data)) {
+        return (
+          <Table
+            columns={Object.keys(data[0] || {}).map((val: string) => ({
+              title: val,
+              dataIndex: val,
+            }))}
+            dataSource={data}
+          />
+        );
+      }
+      return <div>{data}</div>;
+    }
+  }, [data, isLoading]);
+
+  return (
+    <div className="flex flex-col gap-4 justify-start">
+      <textarea
+        className="rounded-md p-5 w-full"
+        rows={2}
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={(e) => (e.key === "Enter" ? runQuery(input) : null)}
+      />
+
+      <div className="flex w-full gap-4 justify-between">
+        <div className="w-1/3">Selected DB: {dbName}</div>
+        <input
+          type="file"
+          accept=".sqlite3"
+          className="w-1/3"
+          onChange={(e) => {
+            updateDB(e.target.files?.[0]);
+            setSelectedDB(e.target.files?.[0]?.name || "");
+            e.target.value = "";
+          }}
+        />
+        <button
+          className="w-1/6"
+          onClick={() =>
+            runQuery("SELECT * FROM sqlite_master WHERE type='table';")
+          }
+        >
+          List Tables
+        </button>
+      </div>
+
+      <div className="w-full">{content}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@sqlite.org/sqlite-wasm"],
+  },
+});
