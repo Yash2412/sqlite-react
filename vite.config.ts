9e9b58cae37c4234518913cddff5709bfcdf5a94
Pertempto <pertemptodisce@gmail.com>
2025-01-01 06:25:19 -0500

    feat: add Dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
